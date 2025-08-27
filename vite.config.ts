8f479934aa4b383e5972951b8b1e3ff6e609e862
Dmitry Kozlov <kozlovdmitryi787@gmail.com>
2025-08-27 09:13:35 +0700

    Адаптация приложения для GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/global-time-dial/',
+  base: mode === 'production' ? '/global-time-dial/' : '/',
   server: {
     host: "::",
     port: 8080,
